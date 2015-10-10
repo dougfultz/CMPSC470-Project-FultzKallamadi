8f4dc7368534ba45681cb2fe25326de89b8d3179
dougfultz <dougfultz@users.noreply.github.com>
2015-10-09 23:48:07 -0400

    P2.java: added case for each symbol type. README.md: updated shortcut.

--- Program 2/P2.java
+++ Program 2/P2.java
@@ -43,14 +43,152 @@ class P2 {
         while ( token.sym != sym.EOF) {
             System.out.print( token.value.linenum + ":" + token.value.colnum + " ");
             switch (token.sym) {
+                case sym.ASG:
+                    System.out.println("\t=");
+                    break;
+                case sym.CAND:
+                    System.out.println("\t&&");
+                    break;
+                case sym.CHARLIT:
+                    System.out.println("\tchar literal(" + ((CSXCharLitToken) token.value).charValue + ")");
+                    break;
+                case sym.COLON:
+                    System.out.println("\t:");
+                    break;
+                case sym.COMMA:
+                    System.out.println("\t,");
+                    break;
+                case sym.COR:
+                    System.out.println("\t||");
+                    break;
+                case sym.DEC:
+                    System.out.println("\t--");
+                    break;
+                case sym.EOF:
+                    System.out.println("\tEOF");
+                    break;
+                case sym.EQ:
+                    System.out.print("\t==");
+                    break;
+                case sym.error:
+                    System.out.print("\terror!");
+                    break;
+                case sym.FLOATLIT:
+                    System.out.println("\tfloat literal(" + ((CSXFloatLitToken) token.value).floatValue + ")");
+                    break;
+                case sym.GEQ:
+                    System.out.println("\t>=");
+                    break;
+                case sym.GT:
+                    System.out.println("\t>");
+                    break;
+                case sym.IDENTIFIER:
+                    System.out.println("\tidentifier(" + ((CSXIdentifierToken) token.value).identifierText + ")");
+                    break;
+                case sym.INC:
+                    System.out.println("\t++");
+                    break;
                 case sym.INTLIT:
                     System.out.println("\tinteger literal(" + ((CSXIntLitToken) token.value).intValue + ")");
+                    break;
+                case sym.LBRACE:
+                    System.out.println("\t{");
+                    break;
+                case sym.LBRACKET:
+                    System.out.println("\t[");
+                    break;
+                case sym.LEQ:
+                    System.out.println("\t<=");
+                    break;
+                case sym.LPAREN:
+                    System.out.println("\t(");
+                    break;
+                case sym.LT:
+                    System.out.println("\t<");
+                    break;
+                case sym.MINUS:
+                    System.out.println("\t-");
+                    break;
+                case sym.NOT:
+                    System.out.println("\t!");
+                    break;
+                case sym.NOTEQ:
+                    System.out.println("\t!=");
                     break;
                 case sym.PLUS:
                     System.out.println("\t+");
                     break;
-                case sym.NOTEQ:
-                    System.out.println("\t!=");
+                case sym.RBRACE:
+                    System.out.println("\t}");
+                    break;
+                case sym.RBRACKET:
+                    System.out.println("\t]");
+                    break;
+                case sym.RPAREN:
+                    System.out.println("\t)");
+                    break;
+                case sym.rw_BOOL:
+                    System.out.println("\tbool");
+                    break;
+                case sym.rw_BREAK:
+                    System.out.println("\tbreak");
+                    break;
+                case sym.rw_CHAR:
+                    System.out.println("\tchar");
+                    break;
+                case sym.rw_CLASS:
+                    System.out.println("\tclass");
+                    break;
+                case sym.rw_CONST:
+                    System.out.println("\tconst");
+                    break;
+                case sym.rw_CONTINUE:
+                    System.out.println("\tcontinue");
+                    break;
+                case sym.rw_ELSE:
+                    System.out.println("\telse");
+                    break;
+                case sym.rw_FALSE:
+                    System.out.println("\tfalse");
+                    break;
+                case sym.rw_FLOAT:
+                    System.out.println("\tfloat");
+                    break;
+                case sym.rw_IF:
+                    System.out.println("\tif");
+                    break;
+                case sym.rw_INT:
+                    System.out.println("\tint");
+                    break;
+                case sym.rw_PRINT:
+                    System.out.println("\tprint");
+                    break;
+                case sym.rw_READ:
+                    System.out.println("\tread");
+                    break;
+                case sym.rw_RETURN:
+                    System.out.println("\treturn");
+                    break;
+                case sym.rw_TRUE:
+                    System.out.println("\ttrue");
+                    break;
+                case sym.rw_VOID:
+                    System.out.println("\tvoid");
+                    break;
+                case sym.rw_WHILE:
+                    System.out.println("\twhile");
+                    break;
+                case sym.SEMI:
+                    System.out.println("\t;");
+                    break;
+                case sym.SLASH:
+                    System.out.println("\t/");
+                    break;
+                case sym.STRLIT:
+                    System.out.println("\tstring literal(" + ((CSXStringLitToken) token.value).stringText + ")");
+                    break;
+                case sym.TIMES:
+                    System.out.println("\t*");
                     break;
                 default:
                     System.out.println("unrecognized token type: " + token.value);

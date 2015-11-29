b02c12dcaee50a67c0615cef634bb071f5f9e5a8
dougfultz <dougfultz@users.noreply.github.com>
2015-11-29 14:47:50 -0500

    Kinds: added other possible kinds and made switch statement less error prone

--- Program 4/Kinds.java
+++ Program 4/Kinds.java
@@ -1,16 +1,26 @@
 class Kinds{
  public static final int Var = 0;
  public static final int Value = 1;
- public static final int Other = 2;
+ public static final int Array = 2;
+ public static final int ScalarParm = 3;
+ public static final int ArrayParm = 4;
+ public static final int Method = 5;
+ public static final int Label = 6;
+ public static final int Other = 7;
 
  Kinds(int i){val = i;}
  Kinds(){val = Other;}
 
  public String toString() {
         switch(val){
-          case 0: return "Var";
-          case 1: return "Value";
-          case 2: return "Other";
+          case Var: return "Var";
+          case Value: return "Value";
+          case Array: return "Array";
+          case ScalarParm: return "ScalarParm";
+          case ArrayParm: return "ArrayParm";
+          case Method: return "Method";
+          case Label: return "Label";
+          case Other: return "Other";
           default: throw new RuntimeException();
         }
  }
